d6169fa15e3509726be7b155a7ccd80cd8d5b04c
Juanpidk <150383660+angieherrerauptc@users.noreply.github.com>
2026-05-22 14:42:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("EducaMente funcionando 🚀");
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+
+    anchor.addEventListener('click', function(e) {
+
+        e.preventDefault();
+
+        const destino = document.querySelector(this.getAttribute('href'));
+
+        destino.scrollIntoView({
+            behavior: 'smooth'
+        });
+
+    });
+
+});
